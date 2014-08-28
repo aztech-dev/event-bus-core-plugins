40d379c99b6e866b9c77785bef8ba2179078d762
Thibaud Fabre <thibaud@aztech.io>
2014-08-28 13:09:23 +0200

    Remove factory methods for extra plugins

--- src/Plugins.php
+++ src/Plugins.php
@@ -7,25 +7,11 @@ use Aztech\Events\Bus\Factory\GenericOptionsDescriptor;
 use Aztech\Events\Bus\GenericPluginFactory;
 use Aztech\Events\Bus\Plugins\File\FileChannelProvider;
 use Aztech\Events\Bus\Plugins\Memory\MemoryChannelProvider;
-use Aztech\Events\Bus\Plugins\Mixpanel\MixpanelChannelProvider;
-use Aztech\Events\Bus\Plugins\Mixpanel\MixpanelOptionsDescriptor;
-use Aztech\Events\Bus\Plugins\Stomp\StompChannelProvider;
-use Aztech\Events\Bus\Plugins\Stomp\StompOptionsDescriptor;
 use Aztech\Events\Bus\Plugins\Socket\SocketChannelProvider;
 use Aztech\Events\Bus\Plugins\Socket\SocketOptionsDescriptor;
-use Aztech\Events\Bus\Plugins\Amqp\AmqpChannelProvider;
-use Aztech\Events\Bus\Plugins\Amqp\AmqpOptionsDescriptor;
 
 class Plugins
 {
-
-    static function loadAmqpPlugin()
-    {
-        Events::addPlugin('amqp', new GenericPluginFactory(function ()
-        {
-            return new AmqpChannelProvider();
-        }, new AmqpOptionsDescriptor()));
-    }
 
     static function loadFilePlugin()
     {
@@ -46,27 +32,11 @@ class Plugins
         }));
     }
 
-    static function loadMixpanelPlugin($name = 'mixpanel')
-    {
-        Events::addPlugin($name, new GenericPluginFactory(function ()
-        {
-            return new MixpanelChannelProvider();
-        }, new MixpanelOptionsDescriptor()));
-    }
-
     static function loadSocketPlugin($name = 'socket')
     {
         Events::addPlugin($name, new GenericPluginFactory(function ()
         {
             return new SocketChannelProvider();
         }, new SocketOptionsDescriptor()));
-    }
-
-    static function loadStompPlugin($name = 'stomp')
-    {
-        Events::addPlugin($name, new GenericPluginFactory(function ()
-        {
-            return new StompChannelProvider();
-        }, new StompOptionsDescriptor()));
     }
 }
